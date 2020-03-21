3ef2e2ae7096a0e31550ef1619470c8bb01ebe7c
kevinanggaaa <60419316+kevinanggaaa@users.noreply.github.com>
2020-03-21 09:04:02 +0700

    Create killer2.c

---
+++ soal2/killer2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/syslog.h>
+#include <sys/types.h>
+#include <limits.h>
+
+int main()
+{
+  pid_t anak_id;
+  anak_id = fork();
+  // If there's error for child creation
+  if (anak_id < 0) {
+    // exit_failure constant is equal to 8
+    exit(8);
+  }
+
+  if (anak_id == 0)
+  {
+    // Exec pkill soal2.exe
+    char *pkillArgv[] = {"pkill", "soal2.exe", NULL};
+    execv("/usr/bin/pkill", pkillArgv);
+  } else {
+    // wait for child program
+    wait(NULL);
+    printf("running rm on file");
+    execl("/bin/rm", "rm", "killer2.exe", NULL);
+    printf("test, still shows after execl");
+  }
+  // Child program
+  // delete exe self
+}
